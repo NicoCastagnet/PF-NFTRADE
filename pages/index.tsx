bcb5e813d3335561ff1cdfac11c33bafe064cb55
Nicolas <nicolascastagnet002@gmail.com>
2022-10-22 15:22:45 -0300

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-50">
+      <h1 className="text-3xl font-medium text-slate-800">Working</h1>
+    </div>
+  )
+}
+
+export default Home
