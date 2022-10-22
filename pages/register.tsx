41c7a17f32bb909de1f071eb6818125f5ec3b5c5
Nicolás Castagnet <54487595+NicoCastagnet@users.noreply.github.com>
2022-10-22 16:05:42 -0300

    Register page (#1)
    * Test commmit
    
    * Test commit

--- pages/register.tsx
+++ pages/register.tsx
@@ -13,6 +13,7 @@ export default function Register() {
           width={''}
         />
       </div> */}
+      <div>test</div>
       <div className="division flex flex-row-reverse text-center items-center justify-center h-screen">
         <div className="derecha flex flex-col-reverse">
           <Image
