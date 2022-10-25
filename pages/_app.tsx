50105aead5ca8cd696c3a9ba3e0dc8f5c5362954
roblesdotdev <httpsnip@gmail.com>
2022-10-25 16:32:48 -0300

    feat: add prisma adapter and fix typos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-
-import { SessionProvider } from "next-auth/react"
+import type { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
