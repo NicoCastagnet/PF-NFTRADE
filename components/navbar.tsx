207cc3f3243a4a7cc9a1373304ba14ea052cc1ae
Nicolas <nicolascastagnet002@gmail.com>
2022-10-26 21:42:42 -0300

    Created footbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,12 @@
 import whiteLogo from '@assets/White.png'
 import SvgBitCoin from '@components/icons/svgBitCoin'
 import SvgCart from '@components/icons/svgCart'
-import SvgSearch from '@components/icons/svgSearch'
 import SvgUser from '@components/icons/svgUser'
 import Image from 'next/image'
 
 export default function NavBar() {
   return (
-    <nav className="navbar__nav bg-slate-900 w-full flex flex-row text-center items-center p-2 pl-8 pr-8 justify-between fixed">
+    <nav className="navbar__nav bg-slate-900 w-full flex flex-row text-center items-center p-2 pl-8 pr-8 justify-between mb-10">
       <div className="navbar__izq flex flex-row items-center">
         <Image src={whiteLogo} alt="white_logo" height={80} width={150} />
         <div className="navbar__searchbar">
@@ -16,11 +15,11 @@ export default function NavBar() {
             type="text"
             placeholder="Search articles, colections & accounts"
           />
-          <SvgSearch
+          {/* <SvgSearch
             width={'20'}
             height={'20'}
             className="absolute left-[14.5rem] top-[2.09rem] fill-slate-400 m-1"
-          />
+          /> */}
         </div>
       </div>
       <div className="navbar__buttons flex flex-row items-center text-white">
