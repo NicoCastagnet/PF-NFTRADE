c6276b917e3daa0ce6f7ef47c998d5efba23012c
roblesdotdev <httpsnip@gmail.com>
2022-10-27 19:18:56 -0300

    update api reponse types

--- pages/api/feed/home.ts
+++ pages/api/feed/home.ts
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import prisma from '@lib/db'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { HomeFeedResponse } from 'types/api-responses'
 
 /* this endpoint is for testing purposes */
 export default async function handler(
   _req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse<HomeFeedResponse>,
 ) {
   const nfts = await prisma.category.findMany({
     select: {
