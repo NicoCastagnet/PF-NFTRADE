101f51b7fab08ba4eed352c6bfeb9521f6c4e89d
Nicolas <nicolascastagnet002@gmail.com>
2022-10-26 21:41:43 -0300

    Added some icons

--- components/icons/svgFacebook.tsx
+++ components/icons/svgFacebook.tsx
@@ -14,15 +14,15 @@ const SvgFacebook = ({ ...props }) => (
       y2={40.615}
       gradientUnits="userSpaceOnUse"
     >
-      <stop offset={0} stopColor="#2aa4f4" />
-      <stop offset={1} stopColor="#007ad9" />
+      <stop offset={0} stopColor={props.color0 || '#2aa4f4'} />
+      <stop offset={1} stopColor={props.color1 || '#007ad9'} />
     </linearGradient>
     <path
       fill="url(#a)"
       d="M24 4C12.954 4 4 12.954 4 24s8.954 20 20 20 20-8.954 20-20S35.046 4 24 4z"
     />
     <path
-      fill="#fff"
+      fill={props.color2 || '#fff'}
       d="M26.707 29.301h5.176l.813-5.258h-5.989v-2.874c0-2.184.714-4.121 2.757-4.121h3.283V12.46c-.577-.078-1.797-.248-4.102-.248-4.814 0-7.636 2.542-7.636 8.334v3.498H16.06v5.258h4.948v14.452c.98.146 1.973.246 2.992.246.921 0 1.82-.084 2.707-.204V29.301z"
     />
   </svg>
