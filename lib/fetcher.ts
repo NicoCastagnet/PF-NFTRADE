6e00fe1dfdb6bf4410b95aa71cdb9c03d51fdfd7
roblesdotdev <httpsnip@gmail.com>
2022-10-29 01:23:38 -0300

    cors next js config/fmt

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,4 @@
 // TODO: update for post data
-const fetcher = (url: string) =>
-  fetch(url).then((res) => res.json())
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default fetcher
