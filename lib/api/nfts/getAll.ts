77ee5857dbf8973ca7c66650188de212adedf669
roblesdotdev <httpsnip@gmail.com>
2022-10-30 21:59:44 -0300

    feat: support server side pagination

--- lib/api/nfts/getAll.ts
+++ lib/api/nfts/getAll.ts
@@ -6,19 +6,23 @@ import type { NftsResponse } from 'types/api-responses'
 interface GetNftsProps {
   limit?: number
   order?: string
+  page?: number
 }
 
 /* this endpoint is for testing purposes */
 export default async function getAllNfts({
   limit,
   order,
+  page,
 }: GetNftsProps = {}): Promise<NftsResponse> {
   const take = limit && !isNaN(+limit) && +limit > 1 ? +limit : undefined
   const orderBy = getOrderBy(order as string)
+  const skip = page && limit ? (page - 1) * limit : undefined
 
   const nfts = await prisma.nft.findMany({
     orderBy,
     take,
+    skip,
     select: {
       id: true,
       name: true,
