d6a5c63729f7b5d8d147afdce22eb4999588a1a6
lukis76 <kiu.manoguerra@gmail.com>
2022-11-02 16:30:10 -0300

    not found

--- pages/[...notFound].tsx
+++ pages/[...notFound].tsx
@@ -1,11 +1,14 @@
-// import Image from 'next/image'
+import NavBar from '@components/navbar/navbar'
+import Image from 'next/image'
 import React from 'react'
+import notFound from '../assets/page-not-found.webp'
 
 const NotFound = () => {
   return (
-    <div className="h-screen w-full flex justify-center items-center">
+    <div className="h-screen w-full flex flex-col justify-center items-center gap-4">
+      <NavBar/>
       <h1 className="text-6xl">Not Found</h1>
-      {/* <Image/> */}
+      <Image src={notFound} height={500} width={500} alt='page not found' className='rounded-3xl'/>
     </div>
   )
 }
