c69a9d5e20abe1a3add767dad8ac17df4d091712
lukis76 <kiu.manoguerra@gmail.com>
2022-11-02 16:31:08 -0300

    not found

--- pages/[...notFound].tsx
+++ pages/[...notFound].tsx
@@ -6,9 +6,15 @@ import notFound from '../assets/page-not-found.webp'
 const NotFound = () => {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center gap-4">
-      <NavBar/>
+      <NavBar />
       <h1 className="text-6xl">Not Found</h1>
-      <Image src={notFound} height={500} width={500} alt='page not found' className='rounded-3xl'/>
+      <Image
+        src={notFound}
+        height={500}
+        width={500}
+        alt="page not found"
+        className="rounded-3xl"
+      />
     </div>
   )
 }
