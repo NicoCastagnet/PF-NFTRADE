219404dff41a74771c67df7270bdfa20279ac61c
JUAN PAJARO REYES <37418783+Bird97@users.noreply.github.com>
2022-11-02 12:46:51 -0500

    ordenamiento-may-men-precio/filtrados-precios-sin-estilo (#87)

--- pages/api/filters/orderByPrice.tsx
+++ pages/api/filters/orderByPrice.tsx
@@ -1,19 +1,26 @@
-import prisma from '@lib/db'
+interface nft {
+  id: string
+  name: string
+  price: number
+  verified: boolean
+  db: boolean
+}
 
-export default async function orderByPrice(order: string) {
-  const nfts = await prisma.nft.findMany()
-  if (order == 'min') {
-    nfts.sort((a, b) => {
+export function orderByPrice(array: nft[], order: string): nft[] {
+  const array2 = [...array]
+  if (order === 'min') {
+    array2.sort((a, b) => {
       if (b.price < a.price) return 1
       if (b.price > a.price) return -1
       return 0
     })
-  } else {
-    nfts.sort((a, b) => {
+  }
+  if (order === 'max') {
+    array2.sort((a, b) => {
       if (b.price > a.price) return 1
       if (b.price < a.price) return -1
       return 0
     })
   }
-  return nfts
+  return array2
 }
