4160355daac77b17bdeb680d6cbf22039afa06a0
Nicolas <nicolascastagnet002@gmail.com>
2022-11-01 22:49:40 -0300

    Dashboard started

--- components/home/faqHome.tsx
+++ components/home/faqHome.tsx
@@ -1,10 +1,11 @@
+import SvgChevronDown from '@components/icons/svgChevronDown'
+import SvgQuestionMark from '@components/icons/svgQuestionMark'
 import {
   Accordion,
   AccordionBody,
   AccordionHeader,
 } from '@material-tailwind/react'
 import { Fragment, useState } from 'react'
-import SvgQuestionMark from '../icons/svgQuestionMark'
 
 const UtilsContainer = () => {
   const [open, setOpen] = useState(0)
@@ -20,18 +21,11 @@ const UtilsContainer = () => {
 
   function Icon({ id, open }) {
     return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
+      <SvgChevronDown
         className={`${
           id === open ? 'rotate-180' : ''
         } h-5 w-5 transition-transform`}
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-      </svg>
+      />
     )
   }
 
