79678502d8e9e7c2db107a935eadc13cd8319640
lukis76 <kiu.manoguerra@gmail.com>
2022-11-02 15:59:53 -0300

    not found

---
+++ pages/[notFound].tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center'>
+      <h1 className='text-6xl'>Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
