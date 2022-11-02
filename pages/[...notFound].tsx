a0e90818606c5f2305e80ee5cb93294b263ced13
lukis76 <kiu.manoguerra@gmail.com>
2022-11-02 16:14:45 -0300

    not found

---
+++ pages/[...notFound].tsx
@@ -0,0 +1,13 @@
+// import Image from 'next/image'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-6xl">Not Found</h1>
+      {/* <Image/> */}
+    </div>
+  )
+}
+
+export default NotFound
