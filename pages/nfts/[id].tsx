1985cb8ea7595ad3121b8f0974fa79634722fe38
Matias <matiasdonato14@gmail.com>
2022-11-03 13:18:45 -0300

    estan los comentarios likes y viewssss

--- pages/nfts/[id].tsx
+++ pages/nfts/[id].tsx
@@ -1,6 +1,7 @@
 import Footer from '@components/footer'
 import SvgCoin from '@components/icons/svgCoin'
 import SvgHeart from '@components/icons/svgHeart'
+import SvgTrash from '@components/icons/svgTrash'
 import SvgViews from '@components/icons/svgViews'
 import NavBar from '@components/navbar/navbar'
 import getNftById from '@lib/api/nfts/getById'
@@ -15,25 +16,10 @@ import { useEffect, useState } from 'react'
 import { toast, Toaster } from 'react-hot-toast'
 
 import type { NftDetailResponse } from 'types/api-responses'
-import { useOpenMenu } from '../../hook/openCartMenu'
 
 interface NftDetailProps {
   nft: NftDetailResponse
 }
-
-const comment1 = {
-  id: 1,
-  content: 'Hola como estas',
-  from_account: { user_name: 'Matias' },
-}
-
-const comment2 = {
-  id: 2,
-  content: 'Zzz',
-  from_account: { user_name: 'Pablo' },
-}
-
-const comments = [comment1, comment2]
 
 const NftDetail: NextPage<NftDetailProps> = ({ nft }) => {
   const { cart, addItem } = useCart()
@@ -54,10 +40,10 @@ const NftDetail: NextPage<NftDetailProps> = ({ nft }) => {
       })
     }
     if (user) putViews()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [nft.id, user?.id])
 
   const categories = nft.categories.map((c) => c.name)
-  console.log(categories)
 
   const [reload, setReload] = useState(false)
   function refreshStates() {
@@ -108,6 +94,34 @@ const NftDetail: NextPage<NftDetailProps> = ({ nft }) => {
         content: comment,
       }),
     })
+
+    nft.comments.push({
+      user: {
+        name: user?.name,
+      },
+      content: comment,
+      isPublished: true,
+    })
+
+    setComment('')
+
+    refreshStates()
+  }
+
+  async function deleteComment(id: string) {
+    fetch('/api/put/commentDelete', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        nftId: nft.id,
+        commentId: id,
+      }),
+    })
+
+    nft.comments = nft.comments.filter((c) => c.id !== id)
+    refreshStates()
   }
 
   return (
@@ -210,54 +224,71 @@ const NftDetail: NextPage<NftDetailProps> = ({ nft }) => {
         <div className="w-[1200px] border-[1px] border-gray-400 rounded-[15px] p-4 mb-4">
           <h3 className="text-[1.4rem]">Comments:</h3>
           <div className="h-[400px] overflow-auto">
-            {comments.length > 0 ? (
-              comments.map((c) => (
-                <div
-                  key={c.id}
-                  className="border-[1px] border-gray-300 rounded-[15px] mt-2"
-                >
-                  <div className="flex bg-slate-100 rounded-[15px] rounded-bl-[0] rounded-br-[0]">
-                    <p className="text-[1rem] mr-2 ml-2">From: </p>
-                    <p className="text-[1rem] font-[500] hover:text-slate-600 cursor-pointer">
-                      {c.from_account.user_name}
-                    </p>
-                  </div>
-                  <hr />
-                  <div className="p-2">
-                    <p className="text-[1rem] lg:text-[1.2rem] ml-2">
-                      {c.content}
-                    </p>
-                  </div>
-                </div>
-              ))
+            {nft.comments.length > 0 ? (
+              nft.comments.map(
+                (c) =>
+                  c.isPublished == true && (
+                    <div
+                      key={c.id}
+                      className="border-[1px] border-gray-300 rounded-[15px] mt-2"
+                    >
+                      <div className="flex justify-between items-center bg-slate-100 rounded-[15px] h-[30px] rounded-bl-[0] rounded-br-[0]">
+                        <div className="flex">
+                          <p className="text-[1rem] mr-2 ml-2">From: </p>
+                          <p className="text-[1rem] font-[500] hover:text-slate-600 cursor-pointer">
+                            {c.user.name}
+                          </p>
+                        </div>
+                        {c.user.id === user?.id && (
+                          <button
+                            className="mr-3 hover:fill-red-600"
+                            onClick={() => deleteComment(c.id)}
+                          >
+                            <SvgTrash width={18} height={18} />
+                          </button>
+                        )}
+                      </div>
+                      <hr />
+                      <div className="p-2">
+                        <p className="text-[1rem] lg:text-[1rem] ml-2">
+                          {c.content}
+                        </p>
+                      </div>
+                    </div>
+                  ),
+              )
             ) : (
               <span> There are no comments yet </span>
             )}
           </div>
         </div>
 
-        <label
-          htmlFor="message"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Write a comment
-        </label>
-        <textarea
-          id="message"
-          rows={4}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-          placeholder="Your message..."
-          maxLength={400}
-          name="comment"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        ></textarea>
-        <button
-          onClick={submitComment}
-          className="border-[1px] border-gray-300 rounded-[10px] mt-2 p-2 bg-orange-400 hover:bg-orange-500 transition-all"
-        >
-          Comment
-        </button>
+        {user && (
+          <div>
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Write a comment
+            </label>
+            <textarea
+              id="message"
+              rows={4}
+              className="resize-none block p-2.5 w-full text-[1rem] text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+              placeholder="Your message..."
+              maxLength={400}
+              name="comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            ></textarea>
+            <button
+              onClick={submitComment}
+              className="border-[1px] border-gray-300 rounded-[10px] mt-2 p-2 bg-orange-400 hover:bg-orange-500 transition-all"
+            >
+              Comment
+            </button>
+          </div>
+        )}
       </div>
 
       <Footer />
