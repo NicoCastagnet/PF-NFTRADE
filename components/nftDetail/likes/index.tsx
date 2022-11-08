d2418ea6eb0b1f60e5f921469abaac4f5670ed25
Nicolás Castagnet <54487595+NicoCastagnet@users.noreply.github.com>
2022-11-08 16:03:34 -0300

    Update index.tsx

--- components/nftDetail/likes/index.tsx
+++ components/nftDetail/likes/index.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import SvgHeart from '@components/icons/svgHeart'
 import SvgLoading from '@components/icons/svgLoading'
 import fetcher from '@lib/fetcher'
