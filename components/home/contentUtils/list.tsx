408d2f484b9eb32abe3cb9a2a64a7def61083804
Nicolas <nicolascastagnet002@gmail.com>
2022-11-08 04:58:27 -0300

    Home & marketplace dark/light mode done.

--- components/home/contentUtils/list.tsx
+++ components/home/contentUtils/list.tsx
@@ -2,18 +2,18 @@ import SvgList from '@components/icons/svgList'
 
 const List = () => {
   return (
-    <div className="box1 bg-slate-600 h-60 max-sm:w-72 max-xl:w-72 max-lg:w-96 w-96  rounded-lg p-6 ease duration-500">
+    <div className="box1 bg-transparent border-2 border-slate-800 dark:bg-[#303339] dark:border-2 dark:border-zinc-600 hover:-translate-y-2 hover:shadow-lg h-60 max-sm:w-72 max-xl:w-72 max-lg:w-96 w-96 rounded-lg p-6 ease duration-500">
       <div className="icon flex justify-center items-center bg-blue-600 h-[3.3rem] w-[3.3rem] rounded-full">
         <SvgList fill="#fff" height="19" width="19" />
       </div>
       <div className="content">
         <p className="text-white font-semibold mt-3">List them for sell</p>
-        <p className="text-slate-400 mt-3 mb-3">
-          List all your NFT&apos;s at our marketplace and start getting money.
+        <p className="text-gray-400 mt-3 mb-3">
+          Post all your NFT&apos;s start getting money.
         </p>
         <button
           type="button"
-          className="text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-600 font-medium rounded-full text-sm py-2 px-7 text-center"
+          className="text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-full text-sm py-2 px-7 text-center transition-all"
         >
           Sell now
         </button>
--- components/home/headerContainer.tsx
+++ components/home/headerContainer.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const HeaderContainer = () => {
   const { data: session } = useSession()
   return (
-    <section className="bg-slate-900  w-full flex justify-center items-center">
+    <section className="bg-slate-900 dark:bg-[#202225] w-full flex justify-center items-center">
       <div className=" text-white w-full flex justify-between items-center px-14 max-sm:px-6 pt-28 max-sm:pt-24 max-w-7xl py-5 ">
         <div className="flex flex-col justify-center items-start">
           {/* ----------------------------------------------------- */}
@@ -32,15 +32,15 @@ const HeaderContainer = () => {
             <Link href="/marketplace">
               <button
                 type="button"
-                className="text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500 font-medium rounded-full text-sm px-9 py-2.5 text-center mr-3 mb-2 max-sm:m-0 max-sm:px-6 max-sm:text-xs"
+                className="bg-blue-600 hover:bg-blue-700 transition-all font-medium rounded-full text-sm px-9 py-3 text-center max-sm:px-6 max-sm:text-xs"
               >
                 Explore now
               </button>
             </Link>
 
             <Link href={`${session ? '/nfts/create' : '/login'}`}>
-              <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-white rounded-full group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-800 ">
-                <span className="relative px-9 py-2.5 transition-all ease-in duration-75 bg-slate-900 rounded-full group-hover:bg-opacity-0 max-sm:m-0 max-sm:px-6 max-sm:text-xs">
+              <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-white rounded-full group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white">
+                <span className="relative px-9 py-2.5 transition-all ease-in duration-75 bg-slate-900 dark:bg-[#202225] rounded-full group-hover:bg-opacity-0 max-sm:m-0 max-sm:px-6 max-sm:text-xs">
                   Create NFT
                 </span>
               </button>
