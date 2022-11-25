f242ce1462ab4ecb2e83d4c42b935e15ea8413b1
Matias <matiasdonato14@gmail.com>
2022-11-25 17:08:14 -0300

    fix cards user

--- pages/api/payments/index.ts
+++ pages/api/payments/index.ts
@@ -8,7 +8,7 @@ import PaymentService from './serviceControllers'
 
 const PaymentInstance = new PaymentController(new PaymentService())
 
-export default async function buy(req: NextApiRequest, res: NextApiResponse) {
+export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const pagos = await PaymentInstance.getPaymentLink(req, res)
     res.status(200).send(pagos)
