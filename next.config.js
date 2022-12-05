91ae1c74cea17d84efc6d3c480e32b0b20915e2d
Matias <matiasdonato14@gmail.com>
2022-12-05 03:10:14 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,38 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      'loremflickr.com',
-      'tdhdjernzsaepxgzandc.supabase.co',
-      'media.licdn.com',
-      'lh3.googleusercontent.com',
-      'pbs.twimg.com',
-    ],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-          { key: 'mode', value: 'no-cors' },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: [
+            'loremflickr.com',
+            'tdhdjernzsaepxgzandc.supabase.co',
+            'media.licdn.com',
+            'lh3.googleusercontent.com',
+            'pbs.twimg.com',
         ],
-      },
-    ]
-  },
+    },
+    async headers() {
+        return [{
+            // matching all API routes
+            source: '/api/:path*',
+            headers: [
+                { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                { key: 'Access-Control-Allow-Origin', value: '*' },
+                {
+                    key: 'Access-Control-Allow-Methods',
+                    value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                },
+                {
+                    key: 'Access-Control-Allow-Headers',
+                    value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                },
+                { key: 'mode', value: 'no-cors' },
+            ],
+        }, ]
+    },
 }
 
 module.exports = nextConfig
