709a23ac81abc03875bea7e8c95b4c2a83d88364
Matias <matiasdonato14@gmail.com>
2022-12-12 07:24:46 -0300

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -75,7 +75,7 @@ export const authOptions: NextAuthOptions = {
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
-//       version: '2.0',
+      //       version: '2.0',
     } as cli),
   ],
   secret: process.env.NEXTAUTH_SECRET,
